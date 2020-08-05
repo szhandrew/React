82a5e39ddce39705d2b013c9187f977af0b19384
Zhaohui Shan <szhandrew@gmail.com>
2020-08-04 23:33:56 -0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -8,9 +9,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,9 +3,9 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 
 
-    function RenderMenuItem({dish,onClick}){
+    function RenderMenuItem({dish}){
         return (
-            <Card onClick={() => onClick(dish.id)}>
+            <Card>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -20,7 +20,7 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
         const menu = props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                  <RenderMenuItem dish={dish} onClick={props.onClick} />
+                  <RenderMenuItem dish={dish} />
               </div>
             );
         });
